7e2716c403035dd9c6d541060f048eceed917af4
sidharthsangelia@gmail. <sidharthsangelia@gmail.com>
2025-04-10 23:17:53 +0530

    ✅ Fix ESLint issue in getDiscoverMovies

--- lib/getMovies.ts
+++ lib/getMovies.ts
@@ -44,13 +44,20 @@ export async function getPopularMovies() {
 }
 export async function getDiscoverMovies(id?: string, keywords?: string) {
   const url = new URL("https://api.themoviedb.org/3/discover/movie");
-  keywords && url.searchParams.set("with_keywords", keywords);
-  id && url.searchParams.set("with_genres", id);
+
+  if (keywords) {
+    url.searchParams.set("with_keywords", keywords);
+  }
+
+  if (id) {
+    url.searchParams.set("with_genres", id);
+  }
 
   const data = await fetchFromTMDB(url);
 
   return data.results;
 }
+
 export async function getSearchedMovies(term:string) {
   const url = new URL("https://api.themoviedb.org/3/search/movie");
   url.searchParams.set("query", term);

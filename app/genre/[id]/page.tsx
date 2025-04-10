d912d0992218fd457acb93a958f22287e192b8a6
sidharthsangelia@gmail. <sidharthsangelia@gmail.com>
2025-04-10 23:30:01 +0530

    Fix: corrected params destructuring for GenrePage

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -4,27 +4,25 @@ import MoviesCarousel from "@/components/MoviesCarousel";
 import { getDiscoverMovies } from "@/lib/getMovies";
 
 type Props = {
-    params: {
-      id: string;
-    };
-    searchParams: {
-      genre?: string;
-    };
+  params: {
+    id: string;
   };
-  
-async function GenrePage({ params: {id}, searchParams : {genre} }:Props) {
-const movies = await getDiscoverMovies(id);
-  
-    return (
-      <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
-            <h1 className="text-6xl font-bold px-10">
-                Results for {genre}
-            </h1>
-        </div>
-        <MoviesCarousel title={"Genre"} movies={movies} isVertical/>
+  searchParams: {
+    genre?: string;
+  };
+};
+
+export default async function GenrePage({ params, searchParams }: Props) {
+  const movies = await getDiscoverMovies(params.id);
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">
+          Results for {searchParams.genre}
+        </h1>
       </div>
-    );
-  }
-  
-  export default GenrePage;
+      <MoviesCarousel title={"Genre"} movies={movies} isVertical />
+    </div>
+  );
+}
